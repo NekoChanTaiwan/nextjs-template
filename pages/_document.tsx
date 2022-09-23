0513f4b0be5086434431f4938c373fdf370007f1
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-09-22 22:29:11 -0700

    move styles to `@layer base`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 function Document() {
   return (
-    <Html lang='en' className='scroll-smooth antialiased'>
+    <Html lang='en'>
       <Head />
-      <body className='overflow-x-hidden bg-gradient-to-br from-[#00b7ff9b] to-[#f43f5d98] text-[#fff] selection:bg-[#67769660]'>
+      <body>
         <Main />
         <NextScript />
       </body>
